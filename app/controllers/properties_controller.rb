9f1c2a029d8df0b945bd46f513c10a6bb3f62d6a
ruegen <ruegen@icloud.com>
2018-07-30 22:05:37 +1000

    add actions to controller

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,1 +1,19 @@
-class Properties
+class Properties < ApplicationController
+
+    before_action :set_property, only: [:show]
+
+    def index
+        @properties = Property.all
+        render json: @properties, include: [:address, :local_government_area]
+    end
+
+    def show  
+        render json: @property, include: [:address, :local_government_area]
+    end
+
+    private
+    def set_property
+        @property = Property.find(params[:id])
+    end
+
+end
