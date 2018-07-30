dde79dfb9953563af498bb59d2c300374b5d9d4a
ruegen <ruegen@icloud.com>
2018-07-30 22:07:32 +1000

    change json routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  scope :api do
+  scope :api, defaults: { format: :json } do
     resources :properties, only: [:index, :show]
   end
 
